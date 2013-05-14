2e064c9cdd8e32d63d44f55b14fc44e97202db42
Sam Harwell <sam@tunnelvisionlabs.com>
2013-05-14 14:01:16 -0500

    Add unit test project with tests for the platform API

---
+++ NOpenCL.Test/TestPlatform.cs
@@ -0,0 +1,67 @@
+﻿namespace NOpenCL.Test
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TestPlatform
+    {
+        [TestMethod]
+        public void TestGetPlatforms()
+        {
+            Platform[] platforms = Platform.GetPlatforms();
+            Assert.IsNotNull(platforms);
+            if (platforms.Length == 0)
+            {
+                Assert.Inconclusive("No OpenCL platforms found.");
+                return;
+            }
+
+            foreach (Platform platform in platforms)
+            {
+                TestPlatformProperties(platform);
+            }
+        }
+
+        private static void TestPlatformProperties(Platform platform)
+        {
+            Assert.IsNotNull(platform);
+
+            Console.WriteLine(platform.ToString());
+
+            string profile = platform.Profile;
+            Assert.IsNotNull(profile);
+            switch (profile)
+            {
+            case "FULL_PROFILE":
+            case "EMBEDDED_PROFILE":
+                break;
+
+            default:
+                Assert.Inconclusive("Unknown platform profile: " + profile);
+                break;
+            }
+
+            string version = platform.Version;
+            StringAssert.Matches(version, new Regex("^OpenCL 1.[12] .*$"));
+
+            string name = platform.Name;
+            Assert.IsNotNull(name);
+            Assert.AreNotEqual(string.Empty, name);
+
+            string vendor = platform.Vendor;
+            Assert.IsNotNull(vendor);
+            Assert.AreNotEqual(string.Empty, vendor);
+
+            IReadOnlyList<string> extensions = platform.Extensions;
+            Assert.IsNotNull(extensions);
+            foreach (string extension in extensions)
+            {
+                Assert.IsNotNull(extension);
+                Assert.AreNotEqual(string.Empty, extension);
+            }
+        }
+    }
+}
