f6d7d9e6be366ecca2940e16afffbc2709f817db
Sam Harwell <sam@tunnelvisionlabs.com>
2013-05-15 15:00:50 -0500

    Support checking the context properties provided by the cl_khr_d3d10_sharing and cl_khr_d3d11_sharing extensions

---
+++ NOpenCL/Extensions/KhrD3D10Sharing.cs
@@ -0,0 +1,43 @@
+﻿namespace NOpenCL.Extensions
+{
+    using System;
+    using System.Linq;
+
+    public static class KhrD3D10Sharing
+    {
+        public static readonly string ExtensionName = "cl_khr_d3d10_sharing";
+
+        /// <summary>
+        /// If the <c>cl_khr_d3d10_sharing</c> extension is enabled, returns true if
+        /// Direct3D 10 resources created as shared by setting MiscFlags to include
+        /// D3D10_RESOURCE_MISC_SHARED will perform faster when shared with OpenCL,
+        /// compared with resources which have not set this flag. Otherwise returns false.
+        /// </summary>
+        public static readonly UnsafeNativeMethods.ContextParameterInfo<bool> D3D10PreferSharedResourcesKhr =
+            (UnsafeNativeMethods.ContextParameterInfo<bool>)new UnsafeNativeMethods.ParameterInfoBoolean(0x402C);
+
+        public static bool IsSupported(Platform platform)
+        {
+            if (platform == null)
+                throw new ArgumentNullException("platform");
+
+            return platform.Extensions.Contains(ExtensionName);
+        }
+
+        public static bool IsSupported(Device device)
+        {
+            if (device == null)
+                throw new ArgumentNullException("device");
+
+            return device.Extensions.Contains(ExtensionName);
+        }
+
+        public static bool GetD3D10PreferSharedResourcesKhr(this Context context)
+        {
+            if (context == null)
+                throw new ArgumentNullException("context");
+
+            return UnsafeNativeMethods.GetContextInfo(context.Handle, D3D10PreferSharedResourcesKhr);
+        }
+    }
+}
---
+++ NOpenCL/Extensions/KhrD3D11Sharing.cs
@@ -0,0 +1,43 @@
+﻿namespace NOpenCL.Extensions
+{
+    using System;
+    using System.Linq;
+
+    public static class KhrD3D11Sharing
+    {
+        public static readonly string ExtensionName = "cl_khr_d3d11_sharing";
+
+        /// <summary>
+        /// If the <c>cl_khr_d3d11_sharing</c> extension is supported, Returns true if
+        /// Direct3D 11 resources created as shared by setting MiscFlags to include
+        /// D3D11_RESOURCE_MISC_SHARED will perform faster when shared with OpenCL,
+        /// compared with resources which have not set this flag. Otherwise returns false.
+        /// </summary>
+        public static readonly UnsafeNativeMethods.ContextParameterInfo<bool> D3D11PreferSharedResourcesKhr =
+            (UnsafeNativeMethods.ContextParameterInfo<bool>)new UnsafeNativeMethods.ParameterInfoBoolean(0x402D);
+
+        public static bool IsSupported(Platform platform)
+        {
+            if (platform == null)
+                throw new ArgumentNullException("platform");
+
+            return platform.Extensions.Contains(ExtensionName);
+        }
+
+        public static bool IsSupported(Device device)
+        {
+            if (device == null)
+                throw new ArgumentNullException("device");
+
+            return device.Extensions.Contains(ExtensionName);
+        }
+
+        public static bool GetD3D11PreferSharedResourcesKhr(this Context context)
+        {
+            if (context == null)
+                throw new ArgumentNullException("context");
+
+            return UnsafeNativeMethods.GetContextInfo(context.Handle, D3D11PreferSharedResourcesKhr);
+        }
+    }
+}
