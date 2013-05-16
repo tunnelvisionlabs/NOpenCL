964d9e6a306855b0ff460bdaa0e4bc1b7bf0ffb2
Sam Harwell <sam@tunnelvisionlabs.com>
2013-05-16 15:41:30 -0500

    Documentation for AddressingMode enumeration

--- NOpenCL/AddressingMode.cs
+++ NOpenCL/AddressingMode.cs
@@ -1,11 +1,44 @@
 ﻿namespace NOpenCL
 {
+    /// <summary>
+    /// Specifies the image addressing-mode i.e. how out-of-range image coordinates are handled.
+    /// <para/>
+    /// For 1D and 2D image arrays, the addressing mode applies only to the x and (x, y)
+    /// coordinates. The addressing mode for the coordinate which specifies the array index
+    /// is always <see cref="ClampToEdge"/>.
+    /// </summary>
     public enum AddressingMode
     {
+        /// <summary>
+        /// For this addressing mode the programmer guarantees that the image coordinates
+        /// used to sample elements of the image refer to a location inside the image;
+        /// otherwise the results are undefined.
+        /// </summary>
         None = 0x1130,
+
+        /// <summary>
+        /// Out-of-range image coordinates are clamped to the extent.
+        /// </summary>
         ClampToEdge = 0x1131,
+
+        /// <summary>
+        /// Out-of-range image coordinates will return a border color. This is similar to
+        /// the GL_ADDRESS_CLAMP_TO_BORDER addressing mode.
+        /// </summary>
         Clamp = 0x1132,
+
+        /// <summary>
+        /// Out-of-range image coordinates are wrapped to the valid range. This address mode
+        /// can only be used with normalized coordinates. If normalized coordinates are not
+        /// used, this addressing mode may generate image coordinates that are undefined.
+        /// </summary>
         Repeat = 0x1133,
+
+        /// <summary>
+        /// Flip the image coordinate at every integer junction. This addressing mode can
+        /// only be used with normalized coordinates. If normalized coordinates are not
+        /// used, this addressing mode may generate image coordinates that are undefined.
+        /// </summary>
         MirroredRepeat = 0x1134,
     }
 }
