bd30bbc5f1a621e64521b34b10a02e6b5518593c
Sam Harwell <sam@tunnelvisionlabs.com>
2015-07-11 13:25:05 -0500

    License the project under the MIT license

--- NOpenCL/Extensions/KhrD3D10Sharing.cs
+++ NOpenCL/Extensions/KhrD3D10Sharing.cs
@@ -1,7 +1,5 @@
-﻿/*
- * Copyright (c) 2013 Sam Harwell, Tunnel Vision Laboratories LLC
- * All rights reserved.
- */
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 namespace NOpenCL.Extensions
 {
